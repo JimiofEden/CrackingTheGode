869edfd4f094f92a3f729de57d32e34ee008883c
jimiofeden <adam.hollock@gmail.com>
2021-10-14 17:48:44 -0400

    001 - Removing duplicates

--- 002_LinkedLists/001_RemoveDupes.go
+++ 002_LinkedLists/001_RemoveDupes.go
@@ -4,13 +4,22 @@ package ch02
  * Given a string, compress it into a readable format
  */
 
-func removeDupes(l *SinglyLinkedList) {
+func removeDupes(l *Node) {
 	n := l.Len()
 	if n <= 1 {
 		return
 	}
 
 	valArr := make(map[int]bool)
-	
-	
+	prev := l
+	for l.next != nil {
+		if _, ok := valArr[l.value]; ok {
+			// Remove node
+			prev.next = l.next
+		} else {
+			valArr[l.value] = true
+			prev = l
+		}
+		l = l.next
+	}
 }
--- 002_LinkedLists/001_RemoveDupes_test.go
+++ 002_LinkedLists/001_RemoveDupes_test.go
@@ -7,24 +7,33 @@ import (
 
 func TestCheckRemoveDupes(t *testing.T) {
 	tests := []struct {
-		l *SinglyLinkedList
-		out *SinglyLinkedList
+		l *Node
+		out *Node
 	}{}
 
-	l1 := NewSinglyLinkedList(1)
+	l0 := NewNode(1)
+	tests = append(tests, struct {
+		l *Node
+		out *Node
+	}{
+		l: l0,
+		out: l0,
+	})
+
+	l1 := NewNode(1)
 	l1.AddTail(2)
 	l1.AddTail(3)
 	l1.AddTail(4)
 	l1.AddTail(5)
 	tests = append(tests, struct {
-		l *SinglyLinkedList
-		out *SinglyLinkedList
+		l *Node
+		out *Node
 	}{
 		l: l1,
 		out: l1,
 	})
 
-	l2 := NewSinglyLinkedList(1)
+	l2 := NewNode(1)
 	l2.AddTail(2)
 	l2.AddTail(2)
 	l2.AddTail(3)
@@ -33,27 +42,27 @@ func TestCheckRemoveDupes(t *testing.T) {
 	l2.AddTail(5)
 
 	tests = append(tests, struct {
-		l *SinglyLinkedList
-		out *SinglyLinkedList
+		l *Node
+		out *Node
 	}{
 		l: l2,
 		out: l1,
 	})
 
-	l3 := NewSinglyLinkedList(1)
+	l3 := NewNode(1)
 
 	tests = append(tests, struct {
-		l *SinglyLinkedList
-		out *SinglyLinkedList
+		l *Node
+		out *Node
 	}{
 		l: l3,
 		out: l3,
 	})
 
-	for _, test := range tests {
+	for i, test := range tests {
 		removeDupes(test.l)
 		if !reflect.DeepEqual(test.l, test.out) {
-			t.Errorf("Test Failed: \n%#v\n Actual result: \n%v\n", test, test.l)
+			t.Errorf("Test %v Failed Expected: \n%#v\n Actual result: \n%v\n", i, test, test.l)
 		}
 	}
 }
